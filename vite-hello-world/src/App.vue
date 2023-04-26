5a5223b123476b10f8763c0e09ab9f96139589a1
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-04-27 00:26:05 +0200

    needed changes

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <AppTitle />
+    <AppHero />
   </div>
 </template>
 
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   name: 'App',
   components: {
     AppTitle,
+    AppHero,
   },
 };
 </script>

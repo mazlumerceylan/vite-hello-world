0c7be49d7972dcca324ba8b9bdd799d063f2bc19
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-04-27 00:23:56 +0200

    first folders and componenets created

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppTitle,
+  },
+};
+</script>
---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>La mia prima app con Vite!</h1>
+</template>
+
+<script>
+export default {
+  name: 'AppTitle',
+};
+</script>

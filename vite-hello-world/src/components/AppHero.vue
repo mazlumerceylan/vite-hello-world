e1886e37281116e919b9f39afa79463c708cced1
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-04-27 00:25:51 +0200

    AppHero created

---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="jumbotron">
+    <h1>Benvenuti nella mia app con Vite!</h1>
+    <p>Questa è la mia prima app creata con Vite.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHero',
+};
+</script>
